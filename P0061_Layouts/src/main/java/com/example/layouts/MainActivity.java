3cbf8191248cc1c4cc3a2965f9b920176cdd8c62
Dmitriy.P <petrukhindmitriy@gmail.com>
2017-10-10 13:42:28 +0300

    commit после объединения уроков

--- P0061_Layouts/src/main/java/com/example/layouts/MainActivity.java
+++ P0061_Layouts/src/main/java/com/example/layouts/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.layoutfiles;
+package com.example.layouts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.myscreen);
+        setContentView(R.layout.activity_main);
     }
 }
--- P0072_LayoutProp/src/main/java/com/example/layoutprop/MainActivity.java
+++ P0072_LayoutProp/src/main/java/com/example/layoutprop/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.layoutfiles;
+package com.example.layoutprop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
